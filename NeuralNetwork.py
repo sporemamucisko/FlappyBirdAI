fd4925376b1976f205449313ee9c9231cc0b99c4
sporemamucisko <sporemamucisko@gmail.com>
2020-02-03 01:51:46 +0100

    Update NeuralNetwork.py

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -15,9 +15,9 @@ class Brain():
         # Creating the neural network
         self.model = Sequential()                                                            #Pusta sieć/struktura do której się dodaje linie -> inizcjalizuje
 
-        self.model.add(Dense(units=32, activation='relu', input_shape=(self.numInputs,)))    #Dodajemy hidden layer, w input shape podajemy liczbę inputów
+        self.model.add(Dense(units=512, activation='relu', input_shape=(self.numInputs,)))    #Dodajemy hidden layer, w input shape podajemy liczbę inputów
 
-        self.model.add(Dense(units=16, activation='relu'))                                   #Dodatkowa hidden layer
+        self.model.add(Dense(units=512, activation='relu'))                                   #Dodatkowa hidden layer
 
         self.model.add(Dense(units=self.numOutputs))                                         #Bez aktywacji bo to ostatnia warstwa(będzie liniowa funkcja aktywacji, może dać tutaj TANH lub tą od 0 do 1)
 
